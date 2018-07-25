ab859025cc03ffdb02ee8c43627b7c6607fa6154
Developer <developer@Users-MacBook-Pro.local>
2018-07-25 15:39:46 +0300

    daily work

---
+++ Assets/Scripts/GameFlow/GUI/Screens/Picker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picker : MonoBehaviour {
+
+    #region Variables
+
+    GameObject picker;
+
+    #endregion
+
+    #region UnityLifeCycle
+
+	void Start () {
+      
+        picker = GetComponent<GameObject>(); 
+        TweenColor.SetColor(picker, Color.red, 1f);
+
+    }
+	    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #endregion
+}
